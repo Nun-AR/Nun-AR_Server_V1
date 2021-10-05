84fed20ede556cb94ed08ee9cc2f746ae596255c
사승은 <62982576+Silver-Deer@users.noreply.github.com>
2021-10-05 14:07:45 +0900

    [INIT] Inital Project

---
+++ src/main/java/com/nunar/nunar/NunArServerV1Application.java
@@ -0,0 +1,13 @@
+package com.nunar.nunar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NunArServerV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NunArServerV1Application.class, args);
+    }
+
+}
